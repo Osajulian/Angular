d2877eefaa2b1551d93b6dae4dae66fdc096fadf
Osajulian <you@example.com>
2018-05-15 00:05:11 -0400

    HTTP & Observables

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
     
     
     private _listFilter : string;
@@ -52,8 +53,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
-        this.filteredProducts = this.products;
+        this._productService.getProducts()
+        .subscribe(products => { 
+            this.products = products;
+            this.filteredProducts = this.products; },
+        error => this.errorMessage = <any>error);
+        
     }
 
 
